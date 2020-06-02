bb8f35cfef9c941da2c7ec1c9ce9145362a7cf2d
zeekling <lingzhaohui@zeekling.cn>
2020-06-02 22:48:27 +0800

    增加面试题64. 求1+2+…+n

---
+++ src/num/SumNums.java
@@ -0,0 +1,20 @@
+package num;
+
+/**
+ * @file SumNums.java
+ * @apiNote 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ *       https://leetcode-cn.com/problems/qiu-12n-lcof/
+ * @author zeekling
+ * @version
+ * @date 2020-06-02
+ */
+public class SumNums{
+
+    public int sumNums(int n) {
+        boolean res = n > 0 && (n += sumNums(n-1)) > 0;
+        return n;
+    }
+
+
+}
+
